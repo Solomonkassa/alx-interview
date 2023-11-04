16f0834657be6ef1675ce2aa39b1fd88bc1c0211
solomon kassa <118729276+Solomonkassa@users.noreply.github.com>
2023-11-04 08:25:58 +0300

    Update 0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,44 +1,59 @@
 #!/usr/bin/python3
-""" N queens """
+"""N Queens"""
 import sys
 
 
-if len(sys.argv) > 2 or len(sys.argv) < 2:
+def print_board(board, n):
+    """Print allocated positions to the queen"""
+    b = []
+
+    for i in range(n):
+        for j in range(n):
+            if j == board[i]:
+                b.append([i, j])
+    print(b)
+
+
+def is_position_safe(board, i, j, r):
+    """Checks if the position is safe for the queen"""
+    return board[i] in (j, j - i + r, i - r + j)
+
+
+def safe_positions(board, row, n):
+    """Find all safe positions where the queen can be allocated"""
+    if row == n:
+        print_board(board, n)
+
+    else:
+        for j in range(n):
+            allowed = True
+            for i in range(row):
+                if is_position_safe(board, i, j, row):
+                    allowed = False
+            if allowed:
+                board[row] = j
+                safe_positions(board, row + 1, n)
+
+
+def create_board(size):
+    """Generates the board"""
+    return [0 * size for i in range(size)]
+
+
+if len(sys.argv) != 2:
     print("Usage: nqueens N")
     exit(1)
 
-if not sys.argv[1].isdigit():
+try:
+    n = int(sys.argv[1])
+except BaseException:
     print("N must be a number")
     exit(1)
 
-if int(sys.argv[1]) < 4:
+if (n < 4):
     print("N must be at least 4")
     exit(1)
 
-n = int(sys.argv[1])
-
-
-def queens(n, i=0, a=[], b=[], c=[]):
-    """ find possible positions """
-    if i < n:
-        for j in range(n):
-            if j not in a and i + j not in b and i - j not in c:
-                yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
-    else:
-        yield a
-
-
-def solve(n):
-    """ solve """
-    k = []
-    i = 0
-    for solution in queens(n, 0):
-        for s in solution:
-            k.append([i, s])
-            i += 1
-        print(k)
-        k = []
-        i = 0
-
-
-solve(n)
+board = create_board(int(n))
+row = 0
+safe_positions(board, row, int(n))
